49cb81da0e43062cd64d6b32ecc329670fdbe33a
abchapagain <achyutchapagain05@gmail.com>
2023-05-05 18:13:54 +0545

    daisyui setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.png'
+// import logo from './logo.png'
 import './App.css'
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className='App'>
-      <h1 className='text-3xl underline'>Hello World</h1>
-    </div>
+    <>
+      <Header />
+
+      <div className='App'>
+        <h1 className='text-3xl underline'>Hello World</h1>
+      </div>
+      <Footer />
+    </>
   )
 }
 
