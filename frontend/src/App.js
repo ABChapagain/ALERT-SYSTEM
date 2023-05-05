2823ab497a3d9966d0aebc6f2b50d80ae260c25e
abchapagain <achyutchapagain05@gmail.com>
2023-05-05 19:28:58 +0545

    header and footer complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,27 @@
 // import logo from './logo.png'
 import './App.css'
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+// Pages import
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Explore from './pages/Explore'
+
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-
-      <div className='App'>
-        <h1 className='text-3xl underline'>Hello World</h1>
-      </div>
+      <main className='min-h-screen'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/explore' element={<Explore />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import { Auth0Provider } from '@auth0/auth0-react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Auth0Provider
-    domain="dev-74ps7j4fsnlvuuel.us.auth0.com"
-    clientId="8Ubo9q3A4j7DEH4IbSbYFHaie79uzU3D"
+    domain='dev-74ps7j4fsnlvuuel.us.auth0.com'
+    clientId='8Ubo9q3A4j7DEH4IbSbYFHaie79uzU3D'
     redirectUri={window.location.origin}
     reloadOnRedirect={false}
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
     }}
   >
     <App />
   </Auth0Provider>
-);
-
-
+)
