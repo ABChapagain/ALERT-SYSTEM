ad222de3a63cad84925f1168ec9b9ab319f3514a
abchapagain <achyutchapagain05@gmail.com>
2023-05-07 04:14:32 +0545

    telegram alert done

--- controllers/alertController.js
+++ controllers/alertController.js
@@ -3,6 +3,16 @@ import Alert from '../models/alertModel.js'
 
 const postAlerts = asyncHandler(async (req, res) => {
   const alerts = await Alert.create(req.body)
+  // pushing to telegram
+
+  let message = `ALERT!!! ${alerts.title}. \n\n ${alerts.message} `
+
+  fetch(
+    `https://api.telegram.org/bot6237365052:AAHOBEefMik-06KVwcPGDXaNCPM_2KtT05c/sendMessage?chat_id=5032853081&text=${message}`
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
   res.send(alerts)
 })
 
